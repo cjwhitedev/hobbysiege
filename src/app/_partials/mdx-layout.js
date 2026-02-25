e52858ac1ed88676c21fe320e30d3e5cba690006
cjwhitedev <hello@cjwhite.dev>
2026-02-25 16:25:44 -0500

    mdx should be working

---
+++ src/app/_partials/mdx-layout.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function MdxLayout({ children }) {
+  // Create any shared layout or styles here
+  return (
+    <div className={styles.page}>
+      <div className="wrapper">
+        <main>{children}</main>
+      </div>
+    </div>
+  )
+}
