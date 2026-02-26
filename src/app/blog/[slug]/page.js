97f66fef769a0a0934b5bd700d99a4100f991def
cjwhitedev <hello@cjwhitedev.com>
2026-02-25 21:43:28 -0500

    static params blog posts with md

---
+++ src/app/blog/[slug]/page.js
@@ -0,0 +1,12 @@
+export default async function Page({ params }) {
+  const { slug } = await params
+  const { default: Post } = await import(`../posts/${slug}.mdx`)
+ 
+  return <Post />
+}
+ 
+export function generateStaticParams() {
+  return [{ slug: 'welcome' }, { slug: 'a-new-post' }]
+}
+ 
+export const dynamicParams = false
