92e6c5499ca47c2029e496a6e6291e9c71d57948
cjwhitedev <hello@cjwhite.dev>
2026-02-27 11:07:37 -0500

    hero conditional

--- src/app/_partials/hero.js
+++ src/app/_partials/hero.js
@@ -2,15 +2,17 @@ import Image from "next/image";
 import './../_styles/hero.css'
 
 export default function Hero({imagePath}) {
-  return (
-        <div className="hero">
-                <Image
-                    className="dithered"
-                    src={imagePath}
-                    alt="example image"
-                    width={600}
-                    height={600}
-                />
-            </div>
-  );
+    if (imagePath) {
+        return (
+          <div className="hero">
+              <Image
+                  className="dithered"
+                  src={imagePath}
+                  alt="example image"
+                  width={600}
+                  height={600}
+              />
+          </div>
+        );
+    }
 }
